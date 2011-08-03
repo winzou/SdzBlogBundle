731ffb2ea1f90f5c19bedf1a77827ef7c1ff2daf
winzou <alexandre.bacco@gmail.com>
2011-08-03 22:43:57 +0800

    Generation de l'entite Article

---
+++ Entity/Article.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace Sdz\BlogBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Sdz\BlogBundle\Entity\Article
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Sdz\BlogBundle\Entity\ArticleRepository")
+ */
+class Article
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var datetime $date
+     *
+     * @ORM\Column(name="date", type="datetime")
+     */
+    private $date;
+
+    /**
+     * @var string $titre
+     *
+     * @ORM\Column(name="titre", type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @var text $contenu
+     *
+     * @ORM\Column(name="contenu", type="text")
+     */
+    private $contenu;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set date
+     *
+     * @param datetime $date
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+    }
+
+    /**
+     * Get date
+     *
+     * @return datetime 
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set titre
+     *
+     * @param string $titre
+     */
+    public function setTitre($titre)
+    {
+        $this->titre = $titre;
+    }
+
+    /**
+     * Get titre
+     *
+     * @return string 
+     */
+    public function getTitre()
+    {
+        return $this->titre;
+    }
+
+    /**
+     * Set contenu
+     *
+     * @param text $contenu
+     */
+    public function setContenu($contenu)
+    {
+        $this->contenu = $contenu;
+    }
+
+    /**
+     * Get contenu
+     *
+     * @return text 
+     */
+    public function getContenu()
+    {
+        return $this->contenu;
+    }
+}
