a744b59fd7007f2d32a37b198be469de760f33a7
winzou <alexandre.bacco@gmail.com>
2011-08-03 21:25:03 +0800

    Creation du bundle avec generate:bundle

---
+++ Controller/DefaultController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Sdz\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class DefaultController extends Controller
+{
+    
+    public function indexAction($name)
+    {
+        return $this->render('SdzBlogBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
